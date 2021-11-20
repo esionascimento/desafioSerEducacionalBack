97a5fb522a1d7ad78ae16a5e864e64cda87e63b5
esionascimento <esio.392@protonmail.com>
2021-11-20 05:17:38 -0400

    create + uma rota para get dados e muito mais na page dashboard

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const loginController = require('../controllers/loginController');
 const controllerUser = require('../controllers/userController');
 const controllerAuthorization = require('../controllers/authorizationController');
-/* const controllerDashboard = require('../controllers/dashboardController'); */
+const controllerDashboard = require('../controllers/dashboardController');
 
 const routers = express.Router();
 
 routers.post('/login', loginController.login);
 routers.use('/user', controllerUser);
 routers.use('/authorization', controllerAuthorization);
-/* routers.use('/dashboard', controllerDashboard); */
+routers.use('/dashboard', controllerDashboard);
 
 module.exports = routers;
