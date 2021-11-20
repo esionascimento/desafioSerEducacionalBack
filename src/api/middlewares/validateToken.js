51c18c505987f99a602c1f1a214f48db17c780e9
esionascimento <esio.392@protonmail.com>
2021-11-20 09:53:31 -0400

    testando novamente deploy

--- src/api/middlewares/validateToken.js
+++ src/api/middlewares/validateToken.js
@@ -2,8 +2,6 @@ const jwt = require('jsonwebtoken');
 require('dotenv');
 
 const validateToken = (token) => {
-  console.log('token :', token);
-
   const SECRET = process.env.SECRET;
 
   const payload = jwt.verify(token, SECRET);
