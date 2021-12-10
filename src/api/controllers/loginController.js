956e695a394ab987b9cc152a75bcaa574abe12f1
esionascimento <esio.392@protonmail.com>
2021-12-10 15:59:43 -0400

    fix console.log

--- src/api/controllers/loginController.js
+++ src/api/controllers/loginController.js
@@ -4,10 +4,12 @@ class loginController {
   constructor(loginService) {
     this.loginService = loginService;
   }
-  login = (req, res) => {
+  login = (req, res, next) => {
     this.loginService.authenticate(req.body)
       .then(user => user ? res.json(user) : res.status(403).json({ message: 'Usuario ou senha invalidos'}))
-      .catch()
+      .catch(() => {
+        return next({status: 401, message: "Erro: Login"});
+      });
   }
 }
 
