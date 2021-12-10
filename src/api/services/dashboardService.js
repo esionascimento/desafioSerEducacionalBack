44f9ae8571bb79130e9c020640d88cb26ab5a452
esionascimento <esio.392@protonmail.com>
2021-12-10 17:50:45 -0400

    implementado funçao para verificar todos os possiveis error ao editar um contato

--- src/api/services/dashboardService.js
+++ src/api/services/dashboardService.js
@@ -1,17 +1,27 @@
-const { createContato, getAllById, deleteContatoService, editContato } = require('../models/dashboardModel');
-
+const { createContato, getAllById, getByName, deleteContatoService, editContato } = require('../models/dashboardModel');
+const { errorContatoExist } = require('../middlewares/constructError');
 require('dotenv').config();
 
 const createContatoService = async (_id, body) => {
-  const {nome, sobrenome, telefone, dataNascimento, endereco, email} = body;
-  const result = await createContato(_id, nome, sobrenome, telefone, dataNascimento, endereco, email)
+  const {name, sobrenome, telefone, dataNascimento, endereco, email} = body;
+  const result = await createContato(_id, name, sobrenome, telefone, dataNascimento, endereco, email)
   return result;
 }
 
-const editContatoService = async (_id, body) => {
+const editContatoService = async (idAgenda, body) => {
   editContato
-  const {nome, sobrenome, telefone, dataNascimento, endereco, email} = body;
-  const result = await editContato(_id, nome, sobrenome, telefone, dataNascimento, endereco, email)
+  const {contato, name, sobrenome, telefone, dataNascimento, endereco, email} = body;
+
+  const notExistContact = await getByName(idAgenda, name);
+
+  if (notExistContact.length > 0) {
+    return errorContatoExist("name already registered");
+  }
+
+  const result = await editContato(idAgenda, contato, name, sobrenome, telefone, dataNascimento, endereco, email);
+  if (result.matchedCount === 0) {
+    return errorContatoExist("Contact not exist");
+  }
   return result;
 }
 
