485405025903b8174d02c38f180600059db90245
esionascimento <esio.392@protonmail.com>
2021-12-10 18:28:44 -0400

    removido funcao nao utilizado para verificao de password

--- src/api/services/dashboardService.js
+++ src/api/services/dashboardService.js
@@ -25,24 +25,4 @@ const editContatoService = async (idAgenda, body) => {
   return result;
 }
 
-/* const authenticate = async ({ email, password }) => {
-  const result = await login(email);
-  if (!result) {
-    return null;
-  }
-  const { _id, name } = result;
-  const { password: hash } = result;
-  if (bcrypt.compareSync(password, hash)) {
-    const token = await jwt.sign({ _id, name, email }, process.env.SECRET, {
-      expiresIn: 3000
-    })
-    return {
-      _id,
-      name,
-      email,
-      token
-    };
-  }
-}; */
-
 module.exports = { createContatoService, getAllById, deleteContatoService, editContatoService };
