e7e85696b7a577147d37e5a69bca92713a176b67
esionascimento <esio.392@protonmail.com>
2021-12-11 16:50:08 -0400

    refatorar tratamento de erros validate token

--- src/api/middlewares/validate.js
+++ src/api/middlewares/validate.js
@@ -4,18 +4,15 @@ require('dotenv').config();
 const validateTokent = (req, res, next) => {
   const token = req.headers.authorization;
   const SECRET = process.env.SECRET;
+  if (!token) {
+    return res.status(401).json({message: 'missing auth token'});
+  }
   try {
-    if (!token) {
-      return res.status(401).json({message: 'missing auth token'});
-    }
     const payload = jwt.verify(token, SECRET);
-    if (!payload) {
-      return next({status: 401, message: 'Invalid authorization'});
-    }
     req.userId = payload._id;
     next();
   } catch (e) {
-    return res.status(401).json({ message: 'jwt malformed' });
+    return res.status(401).json({ message: 'jwt expired' });
   }
 };
 
